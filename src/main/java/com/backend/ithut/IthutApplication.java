716fe24fd6c6d97e0a379c4445023dfb3320f106
NurFullS <nurzhenishov10@gmail.com>
2025-12-04 13:28:37 +0600

    Start project

---
+++ src/main/java/com/backend/ithut/IthutApplication.java
@@ -0,0 +1,13 @@
+package com.backend.ithut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IthutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IthutApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/ithut/IthutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.ithut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IthutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
